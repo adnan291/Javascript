f4eada605118d4fa39d9c53ad1dffc939ff92e09
adnan291 <mohd.adnan.627492@gmail.com>
2023-04-26 18:29:14 +0530

    Get Element By ID

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ console.dir(document);
 console.log(document.domain);
 console.log(document.URL);
 console.log(document.title);
-//document.title= 'ITEMS';
+document.title= 'ITEMS';
 console.log(document.doctype);
 console.log(document.head);
 console.log(document.body);
 console.log(document.all);
-//console.log(document.all[8].textContent="Hello");
+console.log(document.all[8].textContent="Hello");
 console.log(document.forms[0]);
 console.log(document.links);
 console.log(document.images);
 
-Get ELement by ID
+// Get ELement by ID
 var headerTitle=document.getElementById('header-title');
 var header=document.getElementById('main-header');
 console.log(headerTitle);
