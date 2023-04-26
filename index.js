252704003dddb03cf78e13623435cbae972d64fd
adnan291 <mohd.adnan.627492@gmail.com>
2023-04-26 20:27:47 +0530

    Query Selector & Query SelectorAll

--- index.js
+++ index.js
@@ -34,12 +34,46 @@
 // }
 
 //Get Element By Tag Name
-var li=document.getElementsByTagName('li');
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[2].style.backgroundColor='green';
+
+// for(var i=0; i<li.length;i++){
+//     li[i].style.fontWeight='bold';
+// }
+
+// Query Selector
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
+
+// var input=document.querySelector('input');
+// input.value='using query selector';
+
+// var submit=document.querySelector('input[type="submit"]');
+// submit.value="Send";
+
+// var item=document.querySelector('.list-group-item');
+// item.style.color='red';
+
+// var lastItem=document.querySelector('.list-group-item:last-child');
+// lastItem.style.color='purple';
+
+// var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor='green';
+
+// var secondItem=document.querySelector('.list-group-item:nth-child(3)');
+// secondItem.style.color='white';
+
+// Query SelectorAll
+
+var li=document.querySelectorAll('.list-group-item');
 console.log(li);
-console.log(li[1]);
-li[2].style.backgroundColor='green';
 
-for(var i=0; i<li.length;i++){
-    li[i].style.fontWeight='bold';
+li[1].style.color='green';
+
+var odd= document.querySelectorAll('li:nth-child(odd)');
+
+for (let i = 0; i < odd.length; i++) {
+ odd[i].style.backgroundColor='green';   
 }
-
